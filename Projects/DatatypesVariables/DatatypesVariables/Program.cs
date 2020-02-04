a9c73e999560f31774d0de0dbd78812ac4648da0
rebecccc <57101783+rebecccc@users.noreply.github.com>
2020-02-04 08:55:59 -0500

    If Statements (done, but I have issues...)
    I moved the projects folder for VS to my objects github folder and now i cannot debug my code. Could you help me in class? Thank you!

---
+++ Projects/DatatypesVariables/DatatypesVariables/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DatatypesVariables
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
