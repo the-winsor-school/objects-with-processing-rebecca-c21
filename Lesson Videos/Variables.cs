4601b8a1338da6f167f9bb27eb6b899b18835e93
rebecccc <57101783+rebecccc@users.noreply.github.com>
2020-02-04 08:29:39 -0500

    Variables (part 2 notes)
    I learned about different methods and how to create console lines.
    
    For some reason the second time I opened this application in VS it would not allow me to debug, perhaps go over it in class.

---
+++ Lesson Videos/Variables.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Variables
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+
+        	/*
+            int x; //he calls them buckets; we put stuff into them and take stuff out of them 
+            int y; //here, we declare their existence
+            //there is a range of just over two billion for interger variables in c sharp
+
+            x = 7; //the equal sign in this case is known as an assign; 
+            y = x + 3; //it makes whatever is on the left equal to whatever is seen on the right
+            //perfect example of assignment and retrievement of variables
+            
+            Console.WriteLine(y);
+            Console.ReadLine(); 
+            */
+            //for large comments
+
+            Console.WriteLine("What is your name?"); //this method goes to the next line
+            Console.Write("Type your first name: "); //this type of method prints the statement and waits on the line
+            string myFirstName; //we are interested in a STRING of alphanumeric characters, which is why we use this data type
+            myFirstName = Console.ReadLine(); //this allows us to retrieve information from FirstName
+            //never repeat variable names 
+
+            /*
+            string myLastName; //same variable controles as the whole myFirstName variable type
+            Console.Write("Type your last name: ");
+            string myLastName;
+            myLastName = Console.ReadLine();
+            */
+
+            //same as what's right above
+            Console.WriteLine("Type your last name: ");
+            string myLastName = Console.ReadLine(); 
+
+            Console.WriteLine("Hello, " + myFirstName + " " + myLastName); //we're adding strings together
+            Console.ReadLine(); 
+
+        }
+    }
+}
